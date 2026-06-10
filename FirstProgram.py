fe18c683c74d4c0ba9cf1eb98a45241ef026a403
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-10 18:48:35 +0545

    minor

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,8 +1,12 @@
-print("Hellow World");
-print(23+5)
+# print("Hellow World");
+# print(23+5)
 
 
 name = "sagar"
 age = 32
+age2 = age
 price = 23.5
 print(name)
+print("my name is  :", name)
+print("my name is  : " ,age)
+print("my name is  : " ,age2)
