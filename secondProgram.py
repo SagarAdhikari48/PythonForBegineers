26ce9e40b98f52db4c7f9429a225bac083c7ff70
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-11 15:30:04 +0545

    Escape sequence new line in python

--- secondProgram.py
+++ secondProgram.py
@@ -1,3 +1,8 @@
-str1 = "this is a string"
-str2 = """this is a string"""
-str3 = "this is a string"
+# str1 = "this is a string"
+# str2 = """this is a string"""
+# str3 = "this is a string"
+
+
+# Escape Sequences
+str1 = "This is a string.\n We are creating string in python programming!"
+print(str1)
