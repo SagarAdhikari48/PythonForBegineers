0e3b221e825957bee92562687dc594139b9c3eeb
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-11 18:08:49 +0545

    List methods

--- ThirdProgram.py
+++ ThirdProgram.py
@@ -2,7 +2,29 @@
 
 # slicing list
 
-marks = [85, 94, 76, 63, 48]
-print(marks[1 : 4])
-print(marks[1 : ])
-print(marks[-3 : -1])
+# marks = [85, 94, 76, 63, 48]
+# print(marks[1 : 4])
+# print(marks[1 : ])
+# print(marks[-3 : -1])
+
+
+# List Methods
+
+list = [2, 3, 1]
+
+list.append(4)
+print(list)
+
+list.sort()
+print(list)
+
+list.sort(reverse=True)
+print(list)
+
+list.reverse()
+
+
+print(list)
+
+list = list.insert(2, 3)
+print(list)
