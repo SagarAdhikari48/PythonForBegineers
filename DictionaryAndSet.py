53e4525489f532904f7cae20bacc2e8f65dd22c1
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-11 23:47:05 +0545

    Nested dictionary Subject and access it

--- DictionaryAndSet.py
+++ DictionaryAndSet.py
@@ -1,16 +1,28 @@
-info = {
-    "name" : "Sagar",
-    "subjects" : ["Python", "Java", "C++"],
-    "topics" : ("Data Structures", "Algorithms", "OOP"),
+# info = {
+#     "name" : "Sagar",
+#     "subjects" : ["Python", "Java", "C++"],
+#     "topics" : ("Data Structures", "Algorithms", "OOP"),
+#     "age": 35,
+#     "is_adult": True,
+#     "marks" : 80
+# }
+
+# print(info)
+# print(info["name"])
+
+# info["name"] = "Adhikari Sagar"
+# info["newKey"] = "New Value"
+# print(info)
+
+#Nested Dictionary
+student = {
+    "name": "Sagar",
+    "subjects": {"phy": 98, "chem": 99, "maths": 100},
+    "topics": ("Data Structures", "Algorithms", "OOP"),
     "age": 35,
     "is_adult": True,
-    "marks" : 80    
+    "marks": 80,
 }
 
-print(info)
-print(info["name"])
-
-info["name"] = "Adhikari Sagar"
-info["newKey"] = "New Value"
-print(info)
-
+print(student)
+print(student["subjects"]["phy"])
