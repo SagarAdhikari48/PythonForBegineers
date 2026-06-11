78bd75fa398f68863040f7596844a1d6d5369b9b
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-11 18:11:28 +0545

    Tuples in python

--- ThirdProgram.py
+++ ThirdProgram.py
@@ -10,21 +10,22 @@
 
 # List Methods
 
-list = [2, 3, 1]
+# list = [2, 3, 1]
+# list.append(4)
+# print(list)
+# list.sort()
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+# list.reverse()
+# print(list)
+# list = list.insert(2, 3)
+# print(list)
 
-list.append(4)
-print(list)
 
-list.sort()
-print(list)
+#Tuples in python programming
+#- immutable that cannot be change/update as list
+# -tuple are written inside parenthesis
 
-list.sort(reverse=True)
-print(list)
-
-list.reverse()
-
-
-print(list)
-
-list = list.insert(2, 3)
-print(list)
+tup = (2, 1, 3, 1)
+print(type(tup))
