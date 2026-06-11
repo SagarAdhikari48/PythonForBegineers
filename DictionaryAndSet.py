0f1b5330f875f9d72713d69ef57fb010ca230f27
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-11 23:15:30 +0545

    DIctionary key value and key with list and tuples

---
+++ DictionaryAndSet.py
@@ -0,0 +1,10 @@
+info = {
+    "name" : "Sagar",
+    "subjects" : ["Python", "Java", "C++"],
+    "topics" : ("Data Structures", "Algorithms", "OOP"),
+    "age": 35,
+    "is_adult": True,
+    "marks" : 80    
+}
+
+print(info)
