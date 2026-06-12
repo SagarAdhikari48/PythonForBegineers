2bca8f5812ebd4ed5bbb2c40ce6e934b90704824
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-12 19:58:18 +0545

    Continue in while loop

--- 5.Loops.py
+++ 5.Loops.py
@@ -109,3 +109,17 @@ while i <= 5:
     if(i == 3):
         break;
     i += 1
+    
+    
+    
+### Continue 
+# - Used to skip the current iteration and move to next one.
+# - Terminates execution in the current iteration and continues execution of the loop with the next iteration
+
+i = 0
+while i <= 5:
+    if(i == 3):
+        i += 1
+        continue
+    print(i)
+    i +=1
