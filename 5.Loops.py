d04167578837a4453c875bd86e33e218e07344c1
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-12 14:54:11 +0545

    While loops in practice

--- 5.Loops.py
+++ 5.Loops.py
@@ -2,16 +2,38 @@
 # In python - while loop and for loop
 
 # 1. WHILE LOOP
-#Syntax
-    # while condition:
-        #some work
-        
+# Syntax
+# while condition:
+# some work
+
 # while True:  #-> this prints infinitely!
 #     print("Hello Sagar")
 
-count = 1
-while count <= 5 :
+count = 1.0  # iterator
+while count <= 5:
     print("Hello Sagar")
     count += 1
+
+print(count)  # 6
+
+
+# i = 1
+# while i <= 100000:
+#     print("Hello", i)
+#     i += 1
+
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print("Loope ended!")
     
-print(count)
+# Print numbers from 5 to 1
+
+i = 5
+while i >=1:
+    print(i)
+    i -= 1
+    
+print("loops ended!")
