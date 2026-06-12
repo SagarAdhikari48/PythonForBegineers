8bd95979d40059f7d00f96702f172295939f3b55
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-12 14:56:45 +0545

    Q1.print 1 to 100

--- 5.Loops.py
+++ 5.Loops.py
@@ -37,3 +37,12 @@ while i >=1:
     i -= 1
     
 print("loops ended!")
+
+
+### 1. Print numbers from 1 to 100.
+i = 1
+while i <= 100 :
+    print(i)
+    i += 1
+print("Printed from 1 to 100")
+ 
