93b8396a0e6eac557738b06a5e2485bbf342e0d1
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-12 14:38:03 +0545

    Infinite while loop

---
+++ 5.Loops.py
@@ -0,0 +1,10 @@
+# loops are used to repeat instructions.
+# In python - while loop and for loop
+
+# 1. WHILE LOOP
+#Syntax
+    # while condition:
+        #some work
+        
+# while True:  #-> this prints infinitely!
+#     print("Hello Sagar")
