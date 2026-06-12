04cee4190c7acb17e293e1f4e97381f401ddb21e
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-12 16:24:56 +0545

    4.While loop with list and vice versa

--- 5.Loops.py
+++ 5.Loops.py
@@ -28,32 +28,32 @@ while i <= 5:
     print(i)
     i += 1
 print("Loope ended!")
-    
+
 # Print numbers from 5 to 1
 
 i = 5
-while i >=1:
+while i >= 1:
     print(i)
     i -= 1
-    
+
 print("loops ended!")
 
 
 ### 1. Print numbers from 1 to 100.
 i = 1
-while i <= 100 :
+while i <= 100:
     print(i)
     i += 1
 print("Printed from 1 to 100")
 
 ### 2. print numbers from 100 to 1
 i = 100
-while i >= 1 :
+while i >= 1:
     print(i)
     i -= 1
 print("Printed from 100 to 1")
 
-### Print the multiplication of table of number n.
+### 3. Print the multiplication of table of number n.
 
 factor = 1
 number = int(input("enter the number"))
@@ -61,4 +61,25 @@ while factor <= 10:
     mul = number * factor
     factor += 1
     print("the multiplication is ", mul)
- 
+
+### 4. Print the elements of the following list using a loop
+# [1, 4,9, 16, 25, 36, 49, 64, 81, 100]
+
+num = 1
+array = []
+while num <= 10:
+    square = num**2
+    array.append(square)
+    num += 1
+    print("square is :", square)
+
+    print(array)
+    
+    
+    ###ORED print-elemets only
+    nums = [1,4, 9, 16, 25,36, 49, 64, 81,100]
+    idx = 0
+    while idx < len(nums):
+        print(nums[idx]);
+        idx += 1
+    
