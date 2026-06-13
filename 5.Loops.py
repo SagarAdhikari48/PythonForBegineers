383a265bb9bde1ae4b493ddf6c59f89abed3dc08
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-13 17:40:34 +0545

    Factorial using range

--- 5.Loops.py
+++ 5.Loops.py
@@ -261,25 +261,23 @@
 # sum = 0
 # for i in range(1, n + 1):
 #     sum += i
-    
+
 # print(sum)
 
 
-#2.  WAP to find factorial of n number - using for loop
+# 2.  WAP to find factorial of n number - using for loop
 # n = int(input("Enter the number to find the factorial"))
 # fact = 1
 # i = 1
 # while i <= n:
 #     fact *= i
 #     i += 1
-    
+
 # print(fact)
 
 
 n = int(input("Enter the number to find the factorial"))
 fact = 1
-for i in range(1, n+1) :
+for i in range(1, n + 1):
     fact *= i
-print(fact5
-      )    
-
+print(fact)
