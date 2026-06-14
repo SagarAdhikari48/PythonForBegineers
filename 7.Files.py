47eda8527153f8e33884bb32be1912f339dc63ac
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-14 16:51:44 +0545

    File reads in python

---
+++ 7.Files.py
@@ -0,0 +1,8 @@
+## Python can be used to perform operations on a file.(read & write data)
+##types of files 1. Text Files: .txt, .docs, .log etc
+# 2. Binary Files: .mp4, .mov, .png, .jpeg etc
+
+f = open("demo.txt", "r")
+data = f.read()
+print(data)
+print(type(data))
