3f415a2f45e582e7595784dbea081df2b1b9160a
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-16 17:57:00 +0545

    WAP to calculate debit, credit and balances

--- OopsAbstraction&Encapsulation.py
+++ OopsAbstraction&Encapsulation.py
@@ -17,3 +17,36 @@ c1.start()
 
 
 ### so in the above case the details of clutch and acc are not displayed to user called Abstraction and whole program unit called Encapsulation
+
+
+####1. Create Account class with 2 attributes - balance and account no. Create methods for debit, credit and  printing the balance.
+
+class Account:
+     
+     def __init__(self, blc ,acc_no):
+         self.balance = blc
+         self.account_number = acc_no
+         print("hi your account", acc_no, "has ", blc)
+         
+     def debit(self, amount):
+         self.balance -= amount
+         print("Rs", amount, "was debited")
+         print("Total balance = ", self.get_balance())
+         
+     def credit(self, amount):
+         self.balance += amount
+         print("Rs", amount, "was credited")
+         print("Total balance = ", self.get_balance())
+         
+         
+     def get_balance(self):
+         return self.balance
+        
+         
+        
+         
+a1 = Account(350, 12344232)
+a1.debit(30);
+a1.credit(50);
+a1.get_balance()
+
