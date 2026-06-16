af4ce771d4ef59b9269e557d2268b1cfa931faeb
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-16 13:39:11 +0545

    Class and Object concept

---
+++ Oops.py
@@ -0,0 +1,11 @@
+### CLass and Objects in Python 
+# - Class is a blueprint for creating objects- objects cannot create without class
+
+#Creating Class
+class Student:
+    name = "Sagar Adhikari"
+    
+# Creating Object (instance)
+
+s1 = Student() # new object of class
+print(s1.name);
