11fcd7579ffd55bebeda2410eba07b8675184694
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-16 13:43:11 +0545

    creating multiple object of same class

--- Oops.py
+++ Oops.py
@@ -1,11 +1,16 @@
-### CLass and Objects in Python 
+### CLass and Objects in Python
 # - Class is a blueprint for creating objects- objects cannot create without class
 
-#Creating Class
+
+# Creating Class
 class Student:
     name = "Sagar Adhikari"
-    
+
+
 # Creating Object (instance)
 
-s1 = Student() # new object of class
-print(s1.name);
+s1 = Student()  # new object of class
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
