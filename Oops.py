602e306522d79a43847f9e93ffecb4fb15ba904b
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-16 13:46:02 +0545

    Class and object created

--- Oops.py
+++ Oops.py
@@ -3,14 +3,24 @@
 
 
 # Creating Class
-class Student:
-    name = "Sagar Adhikari"
+# class Student:
+#     name = "Sagar Adhikari"
 
 
-# Creating Object (instance)
+# # Creating Object (instance)
 
-s1 = Student()  # new object of class
-print(s1.name)
+# s1 = Student()  # new object of class
+# print(s1.name)
 
-s2 = Student()
-print(s2.name)
+# s2 = Student()
+# print(s2.name)
+
+
+class Car: # THis is how class is created 
+    color = "red"
+    brand = "BYD"
+    
+car1 = Car() # This is how the objects were created!
+print(car1.color)
+print(car1.brand)
+
