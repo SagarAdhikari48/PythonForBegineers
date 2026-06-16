043ed830df4954a4d31ac0b176612f569826ee49
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-16 14:02:17 +0545

    when object is created constructor executed automatically

--- Oops.py
+++ Oops.py
@@ -16,11 +16,26 @@
 # print(s2.name)
 
 
-class Car: # THis is how class is created 
-    color = "red"
-    brand = "BYD"
+# class Car: # THis is how class is created 
+#     color = "red"
+#     brand = "BYD"
     
-car1 = Car() # This is how the objects were created!
-print(car1.color)
-print(car1.brand)
+# car1 = Car() # This is how the objects were created!
+# print(car1.color)
+# print(car1.brand)
 
+
+
+### CONSTRUCTOR - are also called __init__ function - Invoked at the time of object creation -  invoked meand executed
+# All classes have a function called _init_(), which is always executed when the object is being initialized.
+#Example
+
+class Student:
+    name = "Sagar Adhikari"
+
+s1 = Student()
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
+#Here no init function is defined but it automatically creates the init function and executed. there will be always condtructure for us.
