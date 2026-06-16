f5200f926d8f0c88dced632ee635c21117315a31
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-16 11:41:12 +0545

    WAP to create file and write text into it

--- 7.Files.py
+++ 7.Files.py
@@ -92,5 +92,16 @@
 #os.remove(filename)
 
 
-import os
-os.remove("sample.txt")
+# import os
+# os.remove("sample.txt")
+
+
+### 1. Create a new file "practice.txt" using python. Add the following data in it:
+# Hi everyone
+# we are learning File I/O
+# using Java.
+# I like programming in Java
+
+with open("practice.txt" , "w") as f:
+    f.write("Hi everyone \nwe are learning File I/O\n")
+    f.write("using java \nI like programming in java")
