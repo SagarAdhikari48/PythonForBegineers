bdb37ce7dd159bd229609ca89997e9a5e1713d58
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-17 11:22:39 +0545

    Super method- the property of parent class is updated from inherited child class property

--- OopsPart2Inheritance.py
+++ OopsPart2Inheritance.py
@@ -1,11 +1,11 @@
 ### Inheritance -> When one class (child/derived). derives the properties and methods of another class(parent/base).
 # to show inheritance we use parenthesis in child class and take argument  of parent class name in the child class.
 ## Properties -> Attributes and Methods
-#Syntax
- 
+# Syntax
+
 # class Car:
 #     .......
-    
+
 # class ToyataCar(Car):
 #     .........
 
@@ -14,15 +14,15 @@
 #     @staticmethod
 #     def start():
 #         print("Car started...")
-        
+
 #     @staticmethod
 #     def stop():
 #         print("The car stopped...")
-        
-# class ToyataCar(Car):#Inheritance 
+
+# class ToyataCar(Car):#Inheritance
 #     def __init__(self, name):
 #         self.name = name
-        
+
 # car1 = ToyataCar("fortuner")
 # car2 = ToyataCar("prius")
 
@@ -31,43 +31,68 @@
 
 # print(car1.start()) #this is due  inheritance
 # print(car1.color) ## attributes inherit
-   
-   
-   
-### Multilevel inheritance -> Inheritance goes level wise 
-        
+
+
+### Multilevel inheritance -> Inheritance goes level wise
+
 # class Car:
 #     @staticmethod
 #     def start():
 #         print("Car started...")
-        
+
 #     @staticmethod
 #     def stop():
 #         print("The car stopped...")
-        
-# class ToyataCar(Car):#Inheritance 
+
+# class ToyataCar(Car):#Inheritance
 #     def __init__(self, brand):
 #         self.brand = brand
-        
+
 # class Fortuner(ToyataCar):
 #     def __init__(self, type):
 #         self.type = type
-        
+
 # car1 = Fortuner("Diesel")
 # car1.start()
 
 
 ### MULTIPLE INHERITANCE - A child class inherit multiple parent class
-class A:
-    varA = "Welcome to class A"
-    
-class B:
-    varB = "Welcome to class B"
-    
-class C(A, B):
-    varC = "Welcome to class C"
-    
-c1 = C()
-print(c1.varA)
-print(c1.varB)
-print(c1.varC)
+# class A:
+#     varA = "Welcome to class A"
+
+# class B:
+#     varB = "Welcome to class B"
+
+# class C(A, B):
+#     varC = "Welcome to class C"
+
+# c1 = C()
+# print(c1.varA)
+# print(c1.varB)
+# print(c1.varC)
+
+####SUPER METHOD -> whenever we need to call any method in parent class we need to use super
+
+
+class Car:
+
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("Car started...")
+
+    @staticmethod
+    def stop():
+        print("The car stopped...")
+
+
+class ToyataCar(Car):  # Inheritance
+    def __init__(self, name, type):
+        super().__init__(type) # this will pass type to parent class and will not throw wrror
+        self.name = name
+        super().start()
+
+car1 = ToyataCar("pirus", "electric")
+print(car1.type)
