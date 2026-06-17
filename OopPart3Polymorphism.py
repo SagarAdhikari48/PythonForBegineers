84aa115394f3501e8e56634523e51cf7bb615705
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-17 14:33:05 +0545

    Complex number adding in python with operator

--- OopPart3Polymorphism.py
+++ OopPart3Polymorphism.py
@@ -3,11 +3,38 @@
 # Example
 
 
-#### OPERATOR OVERLOADING
+#### OPERATOR OVERLOADING && Dunder Functons
 print(1 + 2)  # 3
 print("sagar" + "Adhikari")  # Concatenate
-print([1, 2, 3] + [4, 5, 6]) # merge
+print([1, 2, 3] + [4, 5, 6])  # merge
 
 
 # The above case is plus operand with differnet uses used for different forms and the output will also be different.
-# this is implicit overloading - the python class already mentioned the first one is adding integer and second one is string concatenate and 3rd one is to merge the list 
+# this is implicit overloading - the python class already mentioned the first one is adding integer and second one is string concatenate and 3rd one is to merge the list
+
+
+##Dunder Function -> dunder function proceeded by double underscore _ are dunder function. in p
+
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def show_number(self):
+        print(self.real, "i +", self.img, "j")
+
+    def add(self, num2):
+        new_real = self.real + num2.real
+        new_img = self.img + num2.img
+        return Complex(new_real, new_img)
+
+
+c1 = Complex(4, 6)
+c1.show_number()
+
+c2 = Complex(3, 4)
+c2.show_number()
+
+c3 = c1.add(c2);
+c3.show_number()
