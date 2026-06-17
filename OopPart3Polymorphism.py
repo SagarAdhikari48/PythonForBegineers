d8188684955fda166eb835bbc439d36dbf31580a
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-17 14:06:52 +0545

    Operator overloading

---
+++ OopPart3Polymorphism.py
@@ -0,0 +1,13 @@
+### POLYMORPHISM - Many + Faces/forms
+# means same things can be used in different ways
+# Example
+
+
+#### OPERATOR OVERLOADING
+print(1 + 2)  # 3
+print("sagar" + "Adhikari")  # Concatenate
+print([1, 2, 3] + [4, 5, 6]) # merge
+
+
+# The above case is plus operand with differnet uses used for different forms and the output will also be different.
+# this is implicit overloading - the python class already mentioned the first one is adding integer and second one is string concatenate and 3rd one is to merge the list 
