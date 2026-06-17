f17e0073ccbd474440afb366737e0f7c18528eb3
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-17 10:03:48 +0545

    Private method and attribute

--- OopsPart2.py
+++ OopsPart2.py
@@ -19,20 +19,20 @@
 # print(s1)  # NameError: name 's1' is not defined
 
 
-###Private attributes and Methods
+###Private attributes and Methods -> private attributes and methods are ment to be used only within the class and are not accessible from outside the class.
 ## we use two underscore to make variable private
 
 
-class Account:
-    def __init__(self, acc_no, acc_pass):
-        self.__acc = acc_no
-        self.__password = acc_pass
+# class Account:
+#     def __init__(self, acc_no, acc_pass):
+#         self.__acc = acc_no
+#         self.__password = acc_pass
 
-    def reset_pass(self):
-        print(self.__password) #can access inside class
+#     def reset_pass(self):
+#         print(self.__password) #can access inside class
 
 
-acc1 = Account("12345", "abcdef")
-print(acc1.reset_pass())
-print(acc1.__acc)  # error 'Account' object has no attribute '__acc'
-print(acc1.__password)  # this cannot access outside class
+# acc1 = Account("12345", "abcdef")
+# print(acc1.reset_pass())
+# print(acc1.__acc)  # error 'Account' object has no attribute '__acc'
+# print(acc1.__password)  # this cannot access outside class
