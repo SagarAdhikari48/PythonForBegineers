9686fafc28ecce75a4066734bb7d21b6c8076f31
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-18 10:56:51 +0545

    Day1- Even or Odd

---
+++ PythonTypescriptSideBySide/DayOne.py
@@ -0,0 +1,9 @@
+#  Challenge #1: Find Even or Odd
+def  find_even_or_odd(number):
+    if(number % 2 == 0):
+        print("Even")
+    else:
+        print("Odd")
+
+find_even_or_odd(10)
+find_even_or_odd(9)
