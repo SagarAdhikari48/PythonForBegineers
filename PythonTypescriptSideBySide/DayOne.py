a6a57b2959af58c507641f1976caf8f54832b122
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-18 11:16:04 +0545

    Day-1-find largest in list

--- PythonTypescriptSideBySide/DayOne.py
+++ PythonTypescriptSideBySide/DayOne.py
@@ -1,9 +1,38 @@
 #  Challenge #1: Find Even or Odd
-def  find_even_or_odd(number):
-    if(number % 2 == 0):
+def find_even_or_odd(number):
+    if number % 2 == 0:
         print("Even")
     else:
         print("Odd")
 
-find_even_or_odd(10)
-find_even_or_odd(9)
+
+# find_even_or_odd(10)
+# find_even_or_odd(9)
+
+
+# Challenge #2
+# Find the largest Number from an array
+def find_largest_number(nums):
+    largest = nums[0]
+    for i in range(0, len(nums)):
+        print(i)
+        if nums[i] > largest:
+            largest = nums[i]
+            i += 1
+    print("The largest number in the list is : ", largest)
+
+
+find_largest_number([4, 10, 7, 99, 5, 120])
+
+# ORED
+
+
+def find_largest(numbers):
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+    return largest
+
+
+print("Largest : ",find_largest([1, 3, 6, 90, 44, 36]))
