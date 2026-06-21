a7df1fec0a3f0cc94580caaf1ee1499c7a43855b
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-21 12:03:00 +0545

    Average of array items

--- PythonTypescriptSideBySide/DayTwo.py
+++ PythonTypescriptSideBySide/DayTwo.py
@@ -10,3 +10,19 @@ def sum_of_array(array):
 
 
 sum_of_array([10, 20, 30, 40])
+
+
+# Challenge #7: Find Average of Numbers
+# [10, 20, 30, 40, 50]
+
+
+def find_average_in_array(arr):
+    sum = 0
+    average = 0
+    for i in range(0, len(arr)):
+        sum += arr[i]
+        average = sum / len(arr)
+    print("average is: ", average)
+
+
+find_average_in_array([10, 20, 30, 40, 50])
